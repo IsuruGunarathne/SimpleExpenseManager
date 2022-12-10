72bc643ef39ff4411553ebb3319bc09ddc327e3b
Isuru Gunarathne <isuru623@gmail.com>
2022-12-10 12:04:10 +0530

    Add dummy data

--- app/src/main/java/lk/ac/mrt/cse/dbs/simpleexpensemanager/control/PersistentExpenseManager.java
+++ app/src/main/java/lk/ac/mrt/cse/dbs/simpleexpensemanager/control/PersistentExpenseManager.java
@@ -8,6 +8,7 @@ import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.DatabaseHelper;
 import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.TransactionDAO;
 import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.impl.PersistentAccountDAO;
 import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.impl.PersistentTransactionDAO;
+import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.model.Account;
 
 public class PersistentExpenseManager extends ExpenseManager{
     private final Context context;
@@ -23,5 +24,13 @@ public class PersistentExpenseManager extends ExpenseManager{
         setTransactionsDAO(transactionDAO);
         AccountDAO accountDAO = new PersistentAccountDAO(databaseHelper);
         setAccountsDAO(accountDAO);
+
+        // dummy data
+        Account dummyAcct1 = new Account("12345A", "Yoda Bank", "Anakin Skywalker", 10000.0);
+        Account dummyAcct2 = new Account("78945Z", "Clone BC", "Obi-Wan Kenobi", 80000.0);
+        getAccountsDAO().addAccount(dummyAcct1);
+        getAccountsDAO().addAccount(dummyAcct2);
+
+        /*** End ***/
     }
 }
